f54deb5f701ca267e6a8aebc48a068853e276608
jaggachouhan-lgtm <jagga.chouhan@gmail.com>
2026-04-16 14:38:22 +0530

    Add files via upload

---
+++ android/app/src/main/java/Application.java
@@ -0,0 +1,11 @@
+package com.panaceasoft.psxmpc;
+
+ import io.flutter.app.FlutterApplication;
+
+ public class Application extends FlutterApplication {
+   @Override
+   public void onCreate() {
+     super.onCreate();
+   }
+
+ }
